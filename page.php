03cb49fa1b6ea792ed35c7b716fde52cacce4214
Nikesh Adhikari <nikeshad254@gmail.com>
2024-09-17 17:48:36 +0545

    child pages and parent pages now show lists of pages and subpages to navigate between them

--- page.php
+++ page.php
@@ -29,15 +29,34 @@ while(have_posts()){
     </div>
     
     <?php
-    }    
+    } 
+
+    $testArray = get_pages(array(
+        'child_of' => get_the_ID()
+    ));
+    if($theParent or $testArray){  
     ?>
-      <!-- <div class="page-links">
-        <h2 class="page-links__title"><a href="#">About Us</a></h2>
+      <div class="page-links">
+        <h2 class="page-links__title"><a href="<?= get_permalink($theParent)?>"><?= get_the_title($theParent)?></a></h2>
+
         <ul class="min-list">
-          <li class="current_page_item"><a href="#">Our History</a></li>
-          <li><a href="#">Our Goals</a></li>
+          <?php 
+            if($theParent != 0){
+                $findChildrenOf = $theParent;
+            }else{
+                $findChildrenOf = get_the_ID();
+            }
+
+            wp_list_pages(array(
+                'title_li' => NULL,
+                'child_of' => $findChildrenOf,
+                'sort_column' => 'menu_order'
+            ));
+          ?>
         </ul>
-      </div> -->
+      </div>
+
+    <?php } ?>
 
       <div class="generic-content">
         <?php the_content();?>
