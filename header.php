663c7d377c6ebb7c67ffc69672d5a279c0fdf208
Nikesh Adhikari <nikeshad254@gmail.com>
2024-09-17 18:27:45 +0545

    added class to custom nav list items if it is active or its children are active

--- header.php
+++ header.php
@@ -19,7 +19,7 @@
             <div class="site-header__menu group">
                 <nav class="main-navigation">
                     <ul>
-                        <li><a href="<?= site_url('/about-us')?>">About Us</a></li>
+                        <li <?php if(is_page('about-us') || wp_get_post_parent_id(0) == 13) echo 'class="current-menu-item"'?>><a href="<?= site_url('/about-us')?>">About Us</a></li>
                         <li><a href="#">Programs</a></li>
                         <li><a href="#">Events</a></li>
                         <li><a href="#">Campuses</a></li>
