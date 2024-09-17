0514ad2143cac49dfd7092e838ed5c33ef84494f
Nikesh Adhikari <nikeshad254@gmail.com>
2024-09-17 16:39:42 +0545

    errors fixed

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 get_header() ;
 ?>
 
+<!-- no 4 complete -->
 <div class="page-banner">
       <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/library-hero.jpg')?>)"></div>
       <div class="page-banner__content container t-center c-white">
