cb9b1073439b34b9df98e8b9e6df9ac7211fd32b
Nikesh Adhikari <nikeshad254@gmail.com>
2024-09-18 18:42:00 +0545

    home page and blog page seperated and seperate blog page setup

--- page.php
+++ page.php
@@ -1,69 +1,69 @@
 <?php
 get_header();
 
-while(have_posts()){
-    the_post();
-    ?>
+while (have_posts()) {
+  the_post();
+?>
 
-<div class="page-banner">
-      <div class="page-banner__bg-image" style="background-image: url(<?= get_theme_file_uri('images/ocean.jpg')?>)"></div>
-      <div class="page-banner__content container container--narrow">
-        <h1 class="page-banner__title"><?php the_title();?></h1>
-        <div class="page-banner__intro">
-          <p>Dont forget to replace me later</p>
-        </div>
+  <div class="page-banner">
+    <div class="page-banner__bg-image" style="background-image: url(<?= get_theme_file_uri('images/ocean.jpg') ?>)"></div>
+    <div class="page-banner__content container container--narrow">
+      <h1 class="page-banner__title"><?php the_title(); ?></h1>
+      <div class="page-banner__intro">
+        <p>Dont forget to replace me later</p>
       </div>
     </div>
+  </div>
 
-    <div class="container container--narrow page-section">
+  <div class="container container--narrow page-section">
 
-    <?php 
+    <?php
     $theParent = wp_get_post_parent_id(get_the_ID());
-    if($theParent != 0){
+    if ($theParent != 0) {
     ?>
 
-    <div class="metabox metabox--position-up metabox--with-home-link">
+      <div class="metabox metabox--position-up metabox--with-home-link">
         <p>
-          <a class="metabox__blog-home-link" href="<?= get_permalink($theParent)?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?= get_the_title($theParent);?></a> <span class="metabox__main"><?php the_title();?></span>
-        </p> 
-    </div>
-    
+          <a class="metabox__blog-home-link" href="<?= get_permalink($theParent) ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?= get_the_title($theParent); ?></a> <span class="metabox__main"><?php the_title(); ?></span>
+        </p>
+      </div>
+
     <?php
-    } 
+    }
 
     $testArray = get_pages(array(
-        'child_of' => get_the_ID()
+      'child_of' => get_the_ID()
     ));
-    if($theParent or $testArray){  
+    if ($theParent or $testArray) {
     ?>
       <div class="page-links">
-        <h2 class="page-links__title"><a href="<?= get_permalink($theParent)?>"><?= get_the_title($theParent)?></a></h2>
+        <h2 class="page-links__title"><a href="<?= get_permalink($theParent) ?>"><?= get_the_title($theParent) ?></a></h2>
 
         <ul class="min-list">
-          <?php 
-            if($theParent != 0){
-                $findChildrenOf = $theParent;
-            }else{
-                $findChildrenOf = get_the_ID();
-            }
+          <?php
+          if ($theParent != 0) {
+            $findChildrenOf = $theParent;
+          } else {
+            $findChildrenOf = get_the_ID();
+          }
 
-            wp_list_pages(array(
-                'title_li' => NULL,
-                'child_of' => $findChildrenOf,
-                'sort_column' => 'menu_order'
-            ));
+          wp_list_pages(array(
+            'title_li' => NULL,
+            'child_of' => $findChildrenOf,
+            'sort_column' => 'menu_order'
+          ));
           ?>
         </ul>
       </div>
 
     <?php } ?>
 
-      <div class="generic-content">
-        <?php the_content();?>
-      </div>
+    <div class="generic-content">
+      <?php the_content(); ?>
     </div>
+  </div>
 
-    <?php
+<?php
 }
 get_footer();
 ?>
