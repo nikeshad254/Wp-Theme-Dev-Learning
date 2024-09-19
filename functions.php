8c982409bfb72870c942dd937098f17c729408f9
Nikesh Adhikari <nikeshad254@gmail.com>
2024-09-19 17:46:06 +0545

    unnecessary files removes as i am not following automation and some scripts setup

--- functions.php
+++ functions.php
@@ -1,12 +1,22 @@
 <?php
 
 // our custom function
-function university_files(){
-    wp_enqueue_script( "main-university-js", get_theme_file_uri("/js/scripts-bundled.js"), NULL, "1.0", true );
-    // a wordpress function that loads css file
-    wp_enqueue_style('custom-google-fonts', "//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i");
-    wp_enqueue_style('font-awesome', "//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css");
+function university_files()
+{
+    wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+    wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+
+    wp_enqueue_script('googleMap', '//maps.googleapis.com/maps/api/js?key=yourkeygoeshere', NULL, '1.0', true);
+    wp_enqueue_script('axios', '//cdnjs.cloudflare.com/ajax/libs/axios/1.7.7/axios.min.js', NULL, '1.0', true);
+    wp_enqueue_script('glidejs', '//cdn.jsdelivr.net/npm/@glidejs/glide', NULL, '1.0', true);
+
+    wp_enqueue_script('main-university-js', get_theme_file_uri('/scripts.js'), array('jquery'), '1.0', true);
     wp_enqueue_style('university_main_styles', get_stylesheet_uri());
+
+    wp_localize_script('main-university-js', 'universityData', array(
+        'root_url' => get_site_url(),
+        'nonce' => wp_create_nonce('wp_rest')
+    ));
 }
 
 // function needs to be called to work (hooks is movement to call the function)
@@ -14,12 +24,11 @@ function university_files(){
 add_action('wp_enqueue_scripts', 'university_files');
 
 
-function university_features(){
+function university_features()
+{
     // this function will add a title tag to the head of the page
     add_theme_support('title-tag');
 }
 
 // call the function after the theme is setup
 add_action("after_setup_theme", "university_features");
-
-?>
