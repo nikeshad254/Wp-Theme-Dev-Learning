f623eb2cde94a1027bc66759c7bef4e1e23227a0
Nikesh Adhikari <nikeshad254@gmail.com>
2024-09-21 19:26:22 +0545

    changed the default query to custom one for event post types

--- functions.php
+++ functions.php
@@ -32,3 +32,23 @@ function university_features()
 
 // call the function after the theme is setup
 add_action("after_setup_theme", "university_features");
+
+function university_adjust_queries($query)
+{
+    if (!is_admin() && is_post_type_archive('event') && $query->is_main_query()) {
+        $today = date('Ymd');
+        $query->set('meta_key', 'event_date');
+        $query->set('orderby', 'meta_value_num');
+        $query->set('order', 'ASC');
+        $query->set('meta_query', array(
+            array(
+                'key' => 'event_date',
+                'compare' => '>=',
+                'value' => $today,
+                'type' => 'numeric'
+            )
+        ));
+    }
+}
+
+add_action('pre_get_posts', 'university_adjust_queries');
