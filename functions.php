5c26f19cd02d2b52d9ac873e65f2a4797d5ede65
Nikesh Adhikari <nikeshad254@gmail.com>
2024-09-26 19:39:27 +0545

    custom thumbnails and text added but actually not working as expected

--- functions.php
+++ functions.php
@@ -34,6 +34,7 @@ function university_features()
     // custom sizes for any features uploaded
     add_image_size('professorLandscape', 400, 260, true);
     add_image_size('professorPortrait', 480, 650, true);
+    add_image_size('pageBanner', 1500, 350, true);
 }
 
 // call the function after the theme is setup
