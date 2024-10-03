361b192d936cf88821b30d78b5b081f646d14197
Nikesh Adhikari <nikeshad254@gmail.com>
2024-10-03 17:27:39 +0545

    search box html moved from footer to script so it loads only when js is on, autofocus on field added

--- scripts.js
+++ scripts.js
@@ -150,6 +150,7 @@
   class Search {
     // 1. describe and create/initiate our object
     constructor() {
+      this.addSearchHTML();
       this.resultsDiv = $("#search-overlay__results");
       this.openButton = $(".js-search-trigger");
       this.closeButton = $(".search-overlay__close");
@@ -219,6 +220,8 @@
     openOverlay() {
       this.searchOverlay.addClass("search-overlay--active");
       $("body").addClass("body-no-scroll");
+      this.searchField.val("");
+      setTimeout(() => this.searchField.focus(), 301);
       this.isOverlayOpen = true;
     }
 
@@ -240,6 +243,25 @@
       if (e.keyCode == 27 && this.isOverlayOpen) {
         this.closeOverlay();
       }
+    }
+
+    addSearchHTML() {
+      $("body").append(`
+        <div class="search-overlay">
+          <div class="search-overlay__top">
+            <div class="container">
+              <i class="fa fa-search search-overlay__icon" aria-hidden="true"></i>
+              <input type="text" class="search-term" placeholder="What are you looking for?" id="search-term">
+              <i class="fa fa-window-close search-overlay__close" aria-hidden="true"></i>
+            </div>
+          </div>
+
+          <div class="container">
+            <div id="search-overlay__results">
+            </div>
+          </div>
+        </div>
+      `);
     }
   }
 
