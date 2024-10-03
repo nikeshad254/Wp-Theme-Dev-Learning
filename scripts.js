5cd03edf35f766d10f1cf3fd0cd94f53f3da0689
Nikesh Adhikari <nikeshad254@gmail.com>
2024-10-03 17:37:22 +0545

    nested json requestes for fetching multiple post types like post and pages in search

--- scripts.js
+++ scripts.js
@@ -196,23 +196,32 @@
           "/wp-json/wp/v2/posts?search=" +
           this.searchField.val(),
         (posts) => {
-          this.resultsDiv.html(`
-            <h2 class="search-overlay__section-title">General Information</h2>
-            ${
-              posts.length
-                ? '<ul class="link-list min-list">'
-                : "<p>No general information matches that search.</p>"
-            }
-            ${posts
-              .map(
-                (post) =>
-                  `<li><a href="${post.link}">${post.title.rendered}</a></li>`
-              )
-              .join("")}
+          $.getJSON(
+            universityData.root_url +
+              "/wp-json/wp/v2/pages?search=" +
+              this.searchField.val(),
+            (pages) => {
+              var combinedResults = posts.concat(pages);
 
-            ${posts.length ? "</ul>" : ""}
-          `);
-          this.isSpinnerVisible = false;
+              this.resultsDiv.html(`
+              <h2 class="search-overlay__section-title">General Information</h2>
+              ${
+                combinedResults.length
+                  ? '<ul class="link-list min-list">'
+                  : "<p>No general information matches that search.</p>"
+              }
+              ${combinedResults
+                .map(
+                  (item) =>
+                    `<li><a href="${item.link}">${item.title.rendered}</a></li>`
+                )
+                .join("")}
+  
+              ${combinedResults.length ? "</ul>" : ""}
+            `);
+              this.isSpinnerVisible = false;
+            }
+          );
         }
       );
     }
