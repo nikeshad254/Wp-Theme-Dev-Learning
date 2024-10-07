ec7b8a7bcafafe24bef40e9a90a2e132fdc243a7
Nikesh Adhikari <nikeshad254@gmail.com>
2024-10-07 18:22:55 +0545

    from custom api feftched professor and events in their own ui

--- scripts.js
+++ scripts.js
@@ -235,6 +235,26 @@
                   ${results.programs.length ? "</ul>" : ""}
 
                   <h2 class="search-overlay__section-title">Professors</h2>
+                  ${
+                    results.professors.length
+                      ? '<ul class="professor-cards">'
+                      : `<p>No Professors matches that search.</p>`
+                  }
+
+                  ${results.professors
+                    .map(
+                      (item) =>
+                        `<li class="professor-card__list-item">
+                            <a class="professor-card" href="${item.permalink}">
+                                <img class="professor-card__image" src="${item.image}" alt="">
+                                <span class="professor-card__name">${item.title}</span>
+                            </a> 
+                        </li>
+                      `
+                    )
+                    .join("")}
+                    
+                  ${results.professors.length ? "</ul>" : ""}
 
                 </div>            
                 <div class="one-third">
@@ -255,6 +275,27 @@
                   ${results.campuses.length ? "</ul>" : ""}
 
                   <h2 class="search-overlay__section-title">Events</h2>
+                  ${
+                    results.events.length
+                      ? ""
+                      : `<p>No Events matches that search. <a href="${universityData.root_url}/events">View all campuses</a></p>`
+                  }
+
+                  ${results.events
+                    .map(
+                      (item) =>
+                        `<div class="event-summary">
+                            <a class="event-summary__date t-center" href="${item.permaLink}">
+                                <span class="event-summary__month">${item.month}</span>
+                                <span class="event-summary__day">${item.day}</span>
+                            </a>
+                            <div class="event-summary__content">
+                                <h5 class="event-summary__title headline headline--tiny"><a href="${item.permaLink}"><?php ${item.title}?></a></h5>
+                                <p>${item.description}<a href="${item.permaLink}" class="nu gray">Learn more</a></p>
+                            </div>
+                        </div>`
+                    )
+                    .join("")}
                 </div>            
               </div>
               `);
