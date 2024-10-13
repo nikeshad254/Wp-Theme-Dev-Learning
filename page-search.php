7a702636f7398aed08b5faf9459d4b5f981900b1
Nikesh Adhikari <nikeshad254@gmail.com>
2024-10-13 17:48:14 +0545

    traditional search complete with templates and new getsearchform method introduced

--- page-search.php
+++ page-search.php
@@ -49,13 +49,7 @@ while (have_posts()) {
         <?php } ?>
 
         <div class="generic-content">
-            <form class="search-form" method="get" action="<?php echo esc_url(site_url('/')); ?>">
-                <label for="s" class="headline headline--medium">Perform a New Search:</label>
-                <div class="search-form-row">
-                    <input placeholder="What are you looking for?" class="s" type=" search" name="s" id="s">
-                    <input class="search-submit" type="submit" value="Search">
-                </div>
-            </form>
+            <?php get_search_form(); ?>
         </div>
     </div>
 
---
+++ searchform.php
@@ -0,0 +1,7 @@
+<form class="search-form" method="get" action="<?php echo esc_url(site_url('/')); ?>">
+    <label for="s" class="headline headline--medium">Perform a New Search:</label>
+    <div class="search-form-row">
+        <input placeholder="What are you looking for?" class="s" type=" search" name="s" id="s">
+        <input class="search-submit" type="submit" value="Search">
+    </div>
+</form>
