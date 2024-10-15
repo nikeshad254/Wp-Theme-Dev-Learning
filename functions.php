02a4a6ea5aa1558f48a017a0098453ef3747deb6
Nikesh Adhikari <nikeshad254@gmail.com>
2024-10-15 19:45:29 +0545

    private notes and sanitization of any input fields for user added

--- functions.php
+++ functions.php
@@ -175,3 +175,20 @@ function ourLoginTitle()
 {
     return get_bloginfo('name');
 }
+
+// force note posts to be private
+add_filter('wp_insert_post_data', 'makeNotePrivate');
+
+function makeNotePrivate($data)
+{
+    if ($data['post_type'] == 'note') {
+        $data['post_content'] = sanitize_textarea_field($data['post_content']);
+        $data['post_title'] = sanitize_text_field($data['post_title']);
+    }
+
+    if ($data['post_type'] == 'note' && $data['post_status'] != 'trash') {
+        $data['post_status'] = "private";
+    }
+
+    return $data;
+}
