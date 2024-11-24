9faeeef0b05df70e5696b72fa3e3b8547dd9017d
Nikesh Adhikari <nikeshad254@gmail.com>
2024-11-24 17:55:54 +0545

    first select field added in the settings page

--- plugins/our-first-plugin/our-first-unique-plugin.php
+++ plugins/our-first-plugin/our-first-unique-plugin.php
@@ -7,11 +7,49 @@ Version: 1.0
 Author: Brad
 Author URI: https://author.com/1234
 */
+
 class WordCountAndTimePlugin
 {
     function __construct()
     {
         add_action('admin_menu', array($this, 'adminPage'));
+        add_action('admin_init', array($this, 'settings'));
+    }
+
+    function settings()
+    {
+        add_settings_section(
+            'wcp_first_section',
+            null,
+            null,
+            'word-count-settings-page'
+        );
+
+        add_settings_field(
+            'wcp_location',
+            'Display Location',
+            array($this, 'locationHTML'),
+            'word-count-settings-page',
+            'wcp_first_section'
+        );
+
+        register_setting(
+            'wordcountplugin',
+            'wcp_location',
+            array(
+                'sanitize_callback' => 'sanitize_text_field',
+                'default' => '0'
+            )
+        );
+    }
+
+    function locationHTML()
+    { ?>
+        <select name="wcp_location">
+            <option value="0">Beginning of Post</option>
+            <option value="1">End of Post</option>
+        </select>
+    <?php
     }
 
     function adminPage()
@@ -27,9 +65,16 @@ class WordCountAndTimePlugin
 
     function ourHTML()
     {
-?>
+    ?>
         <div class="wrap">
             <h1>Word Count Settings</h1>
+            <form action="options.php" method="POST">
+                <?php
+                settings_fields('wordcountplugin'); // Adds the hidden fields
+                do_settings_sections('word-count-settings-page'); // Adds the Form
+                submit_button(); // Actual Submit button
+                ?>
+            </form>
         </div>
 <?php
     }
