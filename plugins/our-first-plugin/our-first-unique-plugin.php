8721f3d02c14abb1fa51141e21c19f8dfdcff57c
Nikesh Adhikari <nikeshad254@gmail.com>
2024-11-24 18:17:18 +0545

    plugins settings api and ui complete with custom sanitization rule on display location

--- plugins/our-first-plugin/our-first-unique-plugin.php
+++ plugins/our-first-plugin/our-first-unique-plugin.php
@@ -8,6 +8,9 @@ Author: Brad
 Author URI: https://author.com/1234
 */
 
+
+// -- 23-06
+
 class WordCountAndTimePlugin
 {
     function __construct()
@@ -25,6 +28,7 @@ class WordCountAndTimePlugin
             'word-count-settings-page'
         );
 
+        //  For Location
         add_settings_field(
             'wcp_location',
             'Display Location',
@@ -37,17 +41,119 @@ class WordCountAndTimePlugin
             'wordcountplugin',
             'wcp_location',
             array(
-                'sanitize_callback' => 'sanitize_text_field',
+                'sanitize_callback' => array($this, 'sanitizeLocation'),
                 'default' => '0'
             )
         );
+
+        // For Headline Text
+        add_settings_field(
+            'wcp_headline',
+            'Headline Text',
+            array($this, 'headlineHTML'),
+            'word-count-settings-page',
+            'wcp_first_section'
+        );
+
+        register_setting(
+            'wordcountplugin',
+            'wcp_headline',
+            array(
+                'sanitize_callback' => 'sanitize_text_field',
+                'default' => 'Post Statistics'
+            )
+        );
+
+        // For Showing WordCount
+        add_settings_field(
+            'wcp_wordcount',
+            'Word Count',
+            array($this, 'checkboxHTML'),
+            'word-count-settings-page',
+            'wcp_first_section',
+            array('theName' => 'wcp_wordcount')
+        );
+
+        register_setting(
+            'wordcountplugin',
+            'wcp_wordcount',
+            array(
+                'sanitize_callback' => 'sanitize_text_field',
+                'default' => '1'
+            )
+        );
+
+        // For Showing Character Count
+        add_settings_field(
+            'wcp_charactercount',
+            'Character Count',
+            array($this, 'checkboxHTML'),
+            'word-count-settings-page',
+            'wcp_first_section',
+            array('theName' => 'wcp_charactercount')
+        );
+
+        register_setting(
+            'wordcountplugin',
+            'wcp_charactercount',
+            array(
+                'sanitize_callback' => 'sanitize_text_field',
+                'default' => '1'
+            )
+        );
+
+        // For Showing Read Time
+        add_settings_field(
+            'wcp_showreadtime',
+            'Read Time',
+            array($this, 'checkboxHTML'),
+            'word-count-settings-page',
+            'wcp_first_section',
+            array('theName' => 'wcp_showreadtime')
+        );
+
+        register_setting(
+            'wordcountplugin',
+            'wcp_showreadtime',
+            array(
+                'sanitize_callback' => 'sanitize_text_field',
+                'default' => '1'
+            )
+        );
+    }
+
+    function sanitizeLocation($input)
+    {
+        if ($input != 0 and $input != 1) {
+            add_settings_error(
+                'wcp_locaion',
+                'wcp_location_error',
+                'Display Location Must Be Either Beginning OR End'
+            );
+            return get_option('wcp_location');
+        }
+        return $input;
+    }
+
+
+    function checkboxHTML($args)
+    {
+?>
+        <input type="checkbox" name="<?= $args['theName']; ?>" value="1" <?php checked(get_option($args['theName'], '1')) ?>>
+    <?php
+    }
+
+    function headlineHTML()
+    { ?>
+        <input type="text" name="wcp_headline" value="<?= esc_attr(get_option('wcp_headline')); ?>">
+    <?php
     }
 
     function locationHTML()
     { ?>
         <select name="wcp_location">
-            <option value="0">Beginning of Post</option>
-            <option value="1">End of Post</option>
+            <option value="0" <?php selected(get_option('wcp_location', '0')) ?>>Beginning of Post</option>
+            <option value="1" <?php selected(get_option('wcp_location', '1')) ?>>End of Post</option>
         </select>
     <?php
     }
