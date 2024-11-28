3d0fc55d31d6af6b6632838953a6810975176dc1
Nikesh Adhikari <nikeshad254@gmail.com>
2024-11-28 20:33:11 +0545

    dynamic save instead of static, with attributes and value changes

--- plugins/are-you-paying-attention/index.php
+++ plugins/are-you-paying-attention/index.php
@@ -17,12 +17,23 @@ class AreYouPayingAttention
 {
     function __construct()
     {
-        add_action('enqueue_block_editor_assets', array($this, 'adminAssets'));
+        add_action('init', array($this, 'adminAssets'));
     }
 
     function adminAssets()
     {
-        wp_enqueue_script('ournewblocktype', plugin_dir_url(__FILE__) . "build/index.js", array('wp-blocks', 'wp-element'));
+        wp_register_script('ournewblocktype', plugin_dir_url(__FILE__) . "build/index.js", array('wp-blocks', 'wp-element'));
+        register_block_type('ourplugin/are-you-paying-attention', array(
+            'editor_script' => 'ournewblocktype',
+            'render_callback' => array($this, 'theHTML')
+        ));
+    }
+
+    function theHTML($attributes)
+    {
+        ob_start(); ?>
+        <h3>Today the sky is <?= esc_html($attributes['skyColor']) ?> and grass is <?= esc_html($attributes['grassColor']) ?></h3>
+<?php return ob_get_clean();
     }
 }
 
--- plugins/are-you-paying-attention/src/index.js
+++ plugins/are-you-paying-attention/src/index.js
@@ -3,20 +3,42 @@ wp.blocks.registerBlockType("ourplugin/are-you-paying-attention", {
   title: "Are You Paying Attention?",
   icon: "smiley",
   category: "Common",
-  edit: function () {
+  attributes: {
+    skyColor: { type: "string" },
+    grassColor: { type: "string" },
+  },
+  edit: function (props) {
+    function updateSkyColor(e) {
+      props.setAttributes({
+        skyColor: e.target.value,
+      });
+    }
+
+    function updateGrassColor(e) {
+      props.setAttributes({
+        grassColor: e.target.value,
+      });
+    }
     // what you will see in admin
     return (
       <div>
-        <p>Hello, this is paragraph</p>
+        <input
+          type="text"
+          placeholder="sky color"
+          value={props.attributes.skyColor}
+          onChange={updateSkyColor}
+        />
+        <input
+          type="text"
+          placeholder="grass color"
+          value={props.attributes.grassColor}
+          onChange={updateGrassColor}
+        />
       </div>
     );
   },
-  save: function () {
+  save: function (props) {
     // what user will see on frontend
-    return (
-      <div>
-        <h3>asjdlsjds</h3>
-      </div>
-    );
+    return null;
   },
 });
