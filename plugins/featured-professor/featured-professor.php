0a007e896290b77f7560903d046ebaa8baeae850
Nikesh Adhikari <nikeshad254@gmail.com>
2024-11-30 18:53:08 +0545

    loaded professor data in front end

--- plugins/featured-professor/featured-professor.php
+++ plugins/featured-professor/featured-professor.php
@@ -7,14 +7,19 @@
   Author URI: https://www.udemy.com/user/bradschiff/
 */
 
-if( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+if (! defined('ABSPATH')) exit; // Exit if accessed directly
 
-class FeaturedProfessor {
-  function __construct() {
+require_once plugin_dir_path(__FILE__) . 'inc/generateProfessorHTML.php';
+
+class FeaturedProfessor
+{
+  function __construct()
+  {
     add_action('init', [$this, 'onInit']);
   }
 
-  function onInit() {
+  function onInit()
+  {
     wp_register_script('featuredProfessorScript', plugin_dir_url(__FILE__) . 'build/index.js', array('wp-blocks', 'wp-i18n', 'wp-editor'));
     wp_register_style('featuredProfessorStyle', plugin_dir_url(__FILE__) . 'build/index.css');
 
@@ -25,10 +30,15 @@ class FeaturedProfessor {
     ));
   }
 
-  function renderCallback($attributes) {
-    return '<p>We will replace this content soon.</p>';
+  function renderCallback($attributes)
+  {
+    if ($attributes['profId']) {
+      wp_enqueue_style('featuredProfessorStyle');
+      return generateProfessorHTML($attributes['profId']);
+    } else {
+      return null;
+    }
   }
-
 }
 
 $featuredProfessor = new FeaturedProfessor();
