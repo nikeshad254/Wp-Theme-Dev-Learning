bd9f9d2507d93d1d87e9a872040a3b9aa36b8022
Nikesh Adhikari <nikeshad254@gmail.com>
2024-11-30 08:49:29 +0545

    made component using preprovided component from wordpress in react

--- plugins/are-you-paying-attention/src/index.js
+++ plugins/are-you-paying-attention/src/index.js
@@ -1,3 +1,13 @@
+import "./index.scss";
+import {
+  TextControl,
+  Flex,
+  FlexBlock,
+  FlexItem,
+  Button,
+  Icon,
+} from "@wordpress/components";
+
 // args: Short Name or Variable Names , Configuration Object
 wp.blocks.registerBlockType("ourplugin/are-you-paying-attention", {
   title: "Are You Paying Attention?",
@@ -7,38 +17,43 @@ wp.blocks.registerBlockType("ourplugin/are-you-paying-attention", {
     skyColor: { type: "string" },
     grassColor: { type: "string" },
   },
-  edit: function (props) {
-    function updateSkyColor(e) {
-      props.setAttributes({
-        skyColor: e.target.value,
-      });
-    }
-
-    function updateGrassColor(e) {
-      props.setAttributes({
-        grassColor: e.target.value,
-      });
-    }
-    // what you will see in admin
-    return (
-      <div>
-        <input
-          type="text"
-          placeholder="sky color"
-          value={props.attributes.skyColor}
-          onChange={updateSkyColor}
-        />
-        <input
-          type="text"
-          placeholder="grass color"
-          value={props.attributes.grassColor}
-          onChange={updateGrassColor}
-        />
-      </div>
-    );
-  },
+  edit: EditComponent,
   save: function (props) {
     // what user will see on frontend
     return null;
   },
 });
+
+function EditComponent(props) {
+  function updateSkyColor(e) {
+    props.setAttributes({
+      skyColor: e.target.value,
+    });
+  }
+
+  function updateGrassColor(e) {
+    props.setAttributes({
+      grassColor: e.target.value,
+    });
+  }
+  // what you will see in admin
+  return (
+    <div className="paying-attention-edit-block">
+      <TextControl label="Question:" />
+      <p>Answers:</p>
+      <Flex>
+        <FlexBlock>
+          <TextControl />
+        </FlexBlock>
+        <FlexItem>
+          <Button>
+            <Icon icon="star-empty" />
+          </Button>
+        </FlexItem>
+        <FlexItem>
+          <Button>Delete</Button>
+        </FlexItem>
+      </Flex>
+    </div>
+  );
+}
