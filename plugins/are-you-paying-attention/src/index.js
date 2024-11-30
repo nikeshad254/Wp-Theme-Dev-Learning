c2ddc64f3b4459cd4c54448284b0c58742a25fec
Nikesh Adhikari <nikeshad254@gmail.com>
2024-11-30 09:01:15 +0545

    ui stying on block

--- plugins/are-you-paying-attention/src/index.js
+++ plugins/are-you-paying-attention/src/index.js
@@ -39,21 +39,25 @@ function EditComponent(props) {
   // what you will see in admin
   return (
     <div className="paying-attention-edit-block">
-      <TextControl label="Question:" />
-      <p>Answers:</p>
+      <TextControl label="Question:" style={{ fontSize: "20px" }} />
+      <p style={{ fontSize: "13px", margin: "20px 0 8px 0" }}>Answers:</p>
       <Flex>
         <FlexBlock>
           <TextControl />
         </FlexBlock>
         <FlexItem>
           <Button>
-            <Icon icon="star-empty" />
+            <Icon icon="star-empty" className="mark-as-correct" />
           </Button>
         </FlexItem>
         <FlexItem>
-          <Button>Delete</Button>
+          <Button isLink className="attention-delete">
+            Delete
+          </Button>
         </FlexItem>
       </Flex>
+
+      <Button isPrimary>Add another answer</Button>
     </div>
   );
 }
