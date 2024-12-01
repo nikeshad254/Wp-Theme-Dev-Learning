008da5b24f934593f81105f11140778a60bdec79
Nikesh Adhikari <nikeshad254@gmail.com>
2024-12-01 11:30:44 +0545

    creating db and fetching data in custom plugin

---
+++ plugins/new-database-table/inc/template-pets.php
@@ -0,0 +1,54 @@
+<?php
+
+get_header(); ?>
+
+<div class="page-banner">
+  <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg'); ?>);"></div>
+  <div class="page-banner__content container container--narrow">
+    <h1 class="page-banner__title">Pet Adoption</h1>
+    <div class="page-banner__intro">
+      <p>Providing forever homes one search at a time.</p>
+    </div>
+  </div>
+</div>
+
+<div class="container container--narrow page-section">
+
+  <p>This page took <strong><?php echo timer_stop(); ?></strong> seconds to prepare. Found <strong>x</strong> results (showing the first x).</p>
+  <?php
+  global $wpdb;
+  $tablename = $wpdb->prefix . "pets";
+  $ourQuery = $wpdb->prepare("SELECT * from $tablename LIMIT 100");
+  $pets = $wpdb->get_results($ourQuery);
+  ?>
+  <table class="pet-adoption-table">
+    <tr>
+      <th>Name</th>
+      <th>Species</th>
+      <th>Weight</th>
+      <th>Birth Year</th>
+      <th>Hobby</th>
+      <th>Favorite Color</th>
+      <th>Favorite Food</th>
+    </tr>
+    <?php
+    foreach ($pets as $pet) {
+    ?>
+      <tr>
+        <td><?= $pet->petname; ?></td>
+        <td><?= $pet->species; ?></td>
+        <td><?= $pet->petweight; ?></td>
+        <td><?= $pet->birthyear; ?></td>
+        <td><?= $pet->favhobby; ?></td>
+        <td><?= $pet->favcolor; ?></td>
+        <td><?= $pet->favfood; ?></td>
+      </tr>
+    <?php
+    }
+    ?>
+
+  </table>
+
+</div>
+
+<?php get_footer(); ?>
